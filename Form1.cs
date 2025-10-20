99f0033f3f69df2a27f97cbec12a4c6911882cb2
JustNikitaHub <nikita1911200418@gmail.com>
2025-10-20 16:19:02 +0300

    Added "Delete button" and "IsCompleted" checkbox

--- Form1.cs
+++ Form1.cs
@@ -9,17 +9,19 @@ namespace TODO_list;
 public partial class Form1 : Form
 {
     //-----------КОМПОНЕНТЫ
-    Button btnNewTask;
-    Button btnEditTask;
-    Button btnSaveJSON;
-    Button btnLoadJSON;
-    ListBox lstTasks;
-    TextBox txtDecriptionTask;
-    Label lblName;
-    Label lblDescr;
+    private Button btnNewTask;
+    private Button btnEditTask;
+    private Button btnDeleteTask;
+    private Button btnSaveJSON;
+    private Button btnLoadJSON;
+    private ListBox lstTasks;
+    private TextBox txtDecriptionTask;
+    private CheckBox cbxIsDone;
+    private Label lblName;
+    private Label lblDescr;
     //-------------
-    TaskManager taskManager;
-    JsonTaskSaver saver;
+    private TaskManager taskManager;
+    private JsonTaskSaver saver;
     public Form1()
     {
         InitializeComponent();
@@ -27,10 +29,12 @@ public partial class Form1 : Form
         Text = "Планировщик заданий";
         InitializeMyControls();
         btnNewTask.Click += btnNewTask_Click;
+        btnDeleteTask.Click += btnDeleteTask_Click;
         btnEditTask.Click += btnEditTask_Click;
         btnSaveJSON.Click += btnSaveJSON_Click;
         btnLoadJSON.Click += btnLoadJSON_Click;
         lstTasks.SelectedIndexChanged += lstTasks_SelectedIndexChanged;
+        cbxIsDone.CheckedChanged += cbxIsDone_CheckedChanged;
         lstTasks.Items.Clear();
         txtDecriptionTask.Text = "";
         saver = new();
@@ -40,11 +44,14 @@ public partial class Form1 : Form
         {
             if (iconStream != null)
             {
-                Icon loadedIcon = new Icon(iconStream);   
+                Icon loadedIcon = new Icon(iconStream);
                 Icon = loadedIcon;
             }
         }
     }
+
+
+
     public void InitializeMyControls()
     {
         btnNewTask = new()
@@ -58,6 +65,14 @@ public partial class Form1 : Form
         {
             Text = "Изменить задачу",
             Location = new Point(btnNewTask.Location.X + btnNewTask.Size.Width + 80, btnNewTask.Location.Y),
+            AutoSize = true,
+            Anchor = AnchorStyles.Left | AnchorStyles.Bottom,
+            Enabled = false
+        };
+        btnDeleteTask = new()
+        {
+            Text = "Удалить задачу",
+            Location = new Point(50, 505),
             AutoSize = true,
             Anchor = AnchorStyles.Left | AnchorStyles.Bottom,
             Enabled = false
@@ -92,24 +107,33 @@ public partial class Form1 : Form
         {
             Text = "Список задач",
             AutoSize = true,
-            Location = new Point(lstTasks.Location.X,lstTasks.Location.Y-25)
+            Location = new Point(lstTasks.Location.X, lstTasks.Location.Y - 25)
         };
         lblDescr = new()
         {
             Text = "Описание задач",
             AutoSize = true,
-            Location = new Point(txtDecriptionTask.Location.X, txtDecriptionTask.Location.Y-25)
+            Location = new Point(txtDecriptionTask.Location.X, txtDecriptionTask.Location.Y - 25)
+        };
+        cbxIsDone = new()
+        {
+            Text = "Завершена",
+            AutoSize = true,
+            Location = new Point(txtDecriptionTask.Location.X, txtDecriptionTask.Location.Y + txtDecriptionTask.Height),
+            Enabled = false
         };
         //НЕ ЗАБЫТЬ ДОБАВИТЬ
         Controls.AddRange(
             lstTasks,
             btnNewTask,
             btnEditTask,
+            btnDeleteTask,
             btnSaveJSON,
-            btnLoadJSON ,
+            btnLoadJSON,
             txtDecriptionTask,
             lblName,
-            lblDescr
+            lblDescr,
+            cbxIsDone
             );
     }
     private void btnNewTask_Click(object sender, EventArgs e)
@@ -131,14 +155,24 @@ public partial class Form1 : Form
         txtDecriptionTask.Text = "";
         Task old = taskManager.FindTask(toDel);
         DateTime saveTime = old.creationTime;
+        bool isDone = old.IsCompleted;
         string newName = Interaction.InputBox("Измените имя", "Редактирование", old.Name);
         string newDecsr = Interaction.InputBox("Измените описание", "Редактирование", old.Description);
 
         //вместо изменения удаляется выбранный Таск и создается копия с новыми данными
         lstTasks.Items.Remove(old.Name);
         taskManager.RemoveTask(old);
-        taskManager.CreateTask(newName, newDecsr, saveTime);
+        taskManager.CreateTask(newName, newDecsr, saveTime, isDone);
         lstTasks.Items.Add(newName);
+    }
+    private void btnDeleteTask_Click(object sender, EventArgs e)
+    {
+        int toDel = lstTasks.SelectedIndex;
+        lstTasks.SelectedIndex = -1;
+        txtDecriptionTask.Text = "";
+        Task old = taskManager.FindTask(toDel);
+        lstTasks.Items.Remove(old.Name);
+        taskManager.RemoveTask(old);
     }
     private void btnSaveJSON_Click(object sender, EventArgs e)
     {
@@ -147,20 +181,38 @@ public partial class Form1 : Form
     private void btnLoadJSON_Click(object sender, EventArgs e)
     {
         saver.Load(taskManager);
+        lstTasks.Items.Clear();
         foreach (var task in taskManager.GetCopy()) lstTasks.Items.Add(task.Name);
     }
     private void lstTasks_SelectedIndexChanged(object sender, EventArgs e)
     {
-        if (lstTasks.SelectedIndex == -1) { btnEditTask.Enabled = false; return; }
+        if (lstTasks.SelectedIndex == -1)
+        {
+            btnEditTask.Enabled = false;
+            btnDeleteTask.Enabled = false;
+            cbxIsDone.Enabled = false;
+            return;
+        }
         btnEditTask.Enabled = true;
-        txtDecriptionTask.Text = taskManager.FindTask(lstTasks.SelectedIndex).Description;
+        btnDeleteTask.Enabled = true;
+        cbxIsDone.Enabled = true;
+        var task = taskManager.FindTask(lstTasks.SelectedIndex);
+        cbxIsDone.CheckedChanged -= cbxIsDone_CheckedChanged;
+        cbxIsDone.Checked = task.IsCompleted;
+        cbxIsDone.CheckedChanged += cbxIsDone_CheckedChanged;
+        txtDecriptionTask.Text = task.Description;
+    }
+    private void cbxIsDone_CheckedChanged(object sender, EventArgs e)
+    {
+        if (cbxIsDone.Checked) taskManager.FindTask(lstTasks.SelectedIndex).MarkCompleted();
+        else taskManager.FindTask(lstTasks.SelectedIndex).MarkUncompleted();
     }
     public class Task
     {
         public int ID { get; init; } = 0;
         public string Name { get; init; } = "Пустая задача";
         public string Description { get; init; } = "Задача не задана";
-        private bool IsCompleted { get; set; } = false;
+        public bool IsCompleted { get; set; } = false;
         public DateTime creationTime { get; init; } = DateTime.Now;
         public Task(string name, string description, int id)
         {
@@ -176,7 +228,7 @@ public partial class Form1 : Form
             ID = id;
             creationTime = date;
         }
-        public Task(){}
+        public Task() { }
         public void MarkCompleted()
         {
             IsCompleted = true;
@@ -184,6 +236,11 @@ public partial class Form1 : Form
         public bool IsTaskCompleted()
         {
             return IsCompleted;
+        }
+
+        public void MarkUncompleted()
+        {
+            IsCompleted = false;
         }
     }
 
@@ -224,9 +281,10 @@ public partial class Form1 : Form
             AddTask(toAdd);
             PushID();
         }
-        public void CreateTask(string name, string description, DateTime date)
+        public void CreateTask(string name, string description, DateTime date, bool isDone=false)
         {
             Task toAdd = new(name, description, nextID, date);
+            if (isDone) toAdd.MarkCompleted();
             AddTask(toAdd);
             PushID();
         }
@@ -248,7 +306,7 @@ public partial class Form1 : Form
         {
             FileName = name;
         }
-        public TaskManager(){}
+        public TaskManager() { }
     }
     public class JsonTaskSaver
     {
@@ -274,9 +332,9 @@ public partial class Form1 : Form
                 tasks = JsonSerializer.Deserialize<List<Task>>(stream);
                 path.Clear();
                 foreach (var task in tasks) path.AddTask(task);
-                MessageBox.Show("Успешная загрузка!","Загрузка задач", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show("Успешная загрузка!", "Загрузка задач", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
-            
+
         }
     }
 }
